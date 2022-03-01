b53c22ab5fbab263d4e971b98521f0203339b796
viktormel <vitekm@ukr.net>
2022-03-01 17:45:47 +0200

    Выполнено задание 2

--- app/src/main/java/come/geekbrains/myapplication/MainActivity.java
+++ app/src/main/java/come/geekbrains/myapplication/MainActivity.java
@@ -1,14 +1,14 @@
 package come.geekbrains.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.main_activity);
+    setContentView(R.layout.adress_activity);
   }
 }
